dd23fb149311349eebc45a089649493e5b308405
Xeonxoo99 <dustn49@gmail.com>
2025-06-21 17:50:50 +0900

    section1 일부 수정

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -5,7 +5,7 @@ function Section1() {
     <section className='relative w-full font-aeonik pt-[6vw] pr-[3vw] pb-[8vw]'>
       <div className='relative inline-block w-[1400px] p-10'>
         <span>
-            <p className='text-[16px]'>프론트엔드 개발자, 항상 발전 중인 사람</p>
+            <p className='absolute text-[16px] font-bold top-6 left-14'>프론트엔드 개발자, 항상 발전 중인 사람.</p>
             <h1 className='p-0 m-0 text-[200px]'
             style={{lineHeight:'0.8em'}}
             >
