f9b03fbbd8845035bbfab518989a7ec462949e70
dustn20230417 <dustn49@gmail.com>
2025-08-20 02:07:56 +0900

    .

--- src/components/Imformation.jsx
+++ src/components/Imformation.jsx
@@ -366,7 +366,7 @@ function Imformation() {
       const lastSectionTimeline = gsap.timeline({
         scrollTrigger: {
           trigger: lastRef.current,
-          start: "top bottom-=5000",
+          start: "top bottom-=6500",
           end: "bottom bottom",
           scrub: 3,
           markers:true,
